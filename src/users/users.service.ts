85f5c3efe26d24505c15b33b9f3ff9e1fa84c606
nenertiy <nenertiy@gmail.com>
2024-11-09 20:30:17 +0300

    fix

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -16,7 +16,6 @@ export class UsersService {
 
   async createUser(email: string, name: string, password: string) {
     const hashedPassword = await bcrypt.hash(password, 10);
-
     return this.prisma.user.create({
       data: { email, name, password: hashedPassword },
     });
