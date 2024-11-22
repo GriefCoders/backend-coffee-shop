ff46877b110aa1c6880b67d7838301f6c78fa975
nenertiy <nenertiy@gmail.com>
2024-11-22 14:29:04 +0300

    update cart

--- src/cart/cart.module.ts
+++ src/cart/cart.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { CartService } from './cart.service';
 import { CartController } from './cart.controller';
 import { PrismaService } from 'src/prisma.service';
+import { CartRepository } from './cart.repository';
 
 @Module({
   controllers: [CartController],
-  providers: [CartService, PrismaService],
+  providers: [CartService, CartRepository, PrismaService],
 })
 export class CartModule {}
